1c4855687de7563afd49f01df673e91fbd7aa473
andrex002 <sergeev-002@yandex.ru>
2020-08-27 23:35:07 +0300

    Задание5.1 Абстрактный класс. Презентер

---
+++ src/utils/task.js
@@ -0,0 +1,30 @@
+const getCurrentDate = () => {
+  const currentDate = new Date();
+  currentDate.setHours(23, 59, 59, 999);
+
+  return new Date(currentDate);
+};
+
+export const isTaskExpired = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = getCurrentDate();
+
+  return currentDate.getTime() > dueDate.getTime();
+};
+
+export const isTaskExpiringToday = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = getCurrentDate();
+
+  return currentDate.getTime() === dueDate.getTime();
+};
+
+export const isTaskRepeating = (repeating) => Object.values(repeating).some(Boolean);
+
+export const humanizeTaskDueDate = (dueDate) => dueDate.toLocaleString(`en-Us`, {day: `numeric`, month: `long`});
