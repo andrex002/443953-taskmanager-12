b0e2d378faed2d36fe133495d717ba7e941ee1fe
andrex002 <sergeev-002@yandex.ru>
2020-08-28 23:31:46 +0300

    Сортировка задач

--- src/utils/task.js
+++ src/utils/task.js
@@ -28,3 +28,39 @@ export const isTaskExpiringToday = (dueDate) => {
 export const isTaskRepeating = (repeating) => Object.values(repeating).some(Boolean);
 
 export const humanizeTaskDueDate = (dueDate) => dueDate.toLocaleString(`en-Us`, {day: `numeric`, month: `long`});
+
+const getWeightForNullDate = (dateA, dateB) => {
+  if (dateA === null && dateB === null) {
+    return 0;
+  }
+
+  if (dateA === null) {
+    return 1;
+  }
+
+  if (dateB === null) {
+    return -1;
+  }
+
+  return null;
+};
+
+export const sortTaskUp = (taskA, taskB) => {
+  const weight = getWeightForNullDate(taskA.dueDate, taskB.dueDate);
+
+  if (weight !== null) {
+    return weight;
+  }
+
+  return taskA.dueDate.getTime() - taskB.dueDate.getTime();
+};
+
+export const sortTaskDown = (taskA, taskB) => {
+  const weight = getWeightForNullDate(taskA.dueDate, taskB.dueDate);
+
+  if (weight !== null) {
+    return weight;
+  }
+
+  return taskB.dueDate.getTime() - taskA.dueDate.getTime();
+};
